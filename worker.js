356461a8cd2ca3d75f5642ce20bcdbab77fd02df
swwind <i@sww.moe>
2024-12-27 20:50:58 +0800

    make fps more steady

--- worker.js
+++ worker.js
@@ -9,6 +9,12 @@ function log(message, color) {
 }
 
 decoder().then((Module) => {
+
+  let frame, frameSize;
+  let consume = 0;
+  let id = 0;
+  let width, height;
+
   onmessage = async (message) => {
     // console.log("worker:", message.data);
     // console.log("worker:", Module);
@@ -16,13 +22,9 @@ decoder().then((Module) => {
     if (message.data.type === "decode") {
       log("loading video...");
 
-      const width = message.data.width;
-      const height = message.data.height;
-      const fps = message.data.fps;
-      const canvas = message.data.offscreen;
-      const ctx = canvas.getContext('2d');
-      canvas.width = width;
-      canvas.height = height;
+      width = message.data.width;
+      height = message.data.height;
+      // const fps = message.data.fps;
 
       const buffer = await (await fetch(message.data.url)).arrayBuffer();
       const ptr = Module._malloc(buffer.byteLength);
@@ -30,43 +32,39 @@ decoder().then((Module) => {
 
       Module._init(ptr, buffer.byteLength);
 
-      const frameSize = message.data.width * message.data.height * 3 / 2;
-      const frame = Module._malloc(frameSize);
+      frameSize = width * height * 3 / 2;
+      frame = Module._malloc(frameSize);
 
-      let consume = 0;
+      postMessage({ type: 'ready' });
+    }
 
-      function nextFrame(id) {
-        const before = Date.now();
+    if (message.data.type === 'frame') {
+      // process next frame
+      const before = Date.now();
 
-        if (!Module._next_frame(frame)) {
-          log(`worker: done, avg ${Math.round(1000 / (consume / id))}fps.`);
-          return;
-        }
-
-        const now = Date.now()
-
-        // enqueue next frame
-        setTimeout(nextFrame, before + 1_000 / fps - now, id + 1);
-        consume += now - before;
-
-        log(`frame#${id}: consume ${now - before}ms, avg ${Math.round(1000 / (consume / (id + 1)))}fps`);
-
-        const frameBuffer = Module.HEAPU8.subarray(frame, frame + frameSize);
-        const videoFrame = new VideoFrame(frameBuffer, {
-          'codedWidth': width,
-          'codedHeight': height,
-          timestamp: id * (1_000 / fps),
-          'format': 'I420',
-        });
-
-        ctx.drawImage(videoFrame, 0, 0);
-        videoFrame.close();
-
-        // postMessage({ type: 'frame', frame: videoFrame }, [videoFrame]);
+      if (!Module._next_frame(frame)) {
+        log(`done, avg ${Math.round(1000 / (consume / id))}fps.`);
+        postMessage({ type: 'end' });
+        return;
       }
 
-      nextFrame(0)
+      const now = Date.now()
+
+      consume += now - before;
+
+      log(`frame#${id}: consume ${now - before}ms, avg ${Math.round(1000 / (consume / (++id)))}fps`);
+
+      const frameBuffer = Module.HEAPU8.subarray(frame, frame + frameSize);
+      const videoFrame = new VideoFrame(frameBuffer, {
+        codedWidth: width,
+        codedHeight: height,
+        timestamp: 0,
+        format: 'I420',
+      });
+
+      postMessage({ type: 'frame', frame: videoFrame }, [videoFrame]);
     }
+
   };
 
   postMessage({ type: "initialize" });
